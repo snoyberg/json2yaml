0082ee4b5c56425d6c4bc6b19c91a1867895e01a
Michael Snoyman <michael@snoyman.com>
2009-12-16 20:17:11 +0200

    Migrated to Json/YamlDoc entirely

--- json2yaml.hs
+++ json2yaml.hs
@@ -1,42 +1,34 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
 import System.Environment (getArgs)
-import Text.Yaml
 import Data.Object.Json
 import Data.Object.Yaml
 import Control.Monad
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
-import qualified Data.ByteString.Char8 as B8
-import Data.Convertible.Text
 import Data.Attempt
-
-showRational :: Rational -> B.ByteString
-showRational = convertSuccess . show . (fromRational :: Rational -> Double)
 
 instance ConvertSuccess JsonScalar YamlScalar where
     convertSuccess (JsonString bs) = YamlScalar bs StrTag Any
-    convertSuccess (JsonNumber n) = YamlScalar (showRational n) FloatTag Any
-    convertSuccess (JsonBoolean True)  = YamlScalar (B8.pack "y") BoolTag Any
-    convertSuccess (JsonBoolean False) = YamlScalar (B8.pack "n") BoolTag Any
-    convertSuccess JsonNull = YamlScalar (B8.pack "~") NullTag Any
-instance ConvertSuccess B8.ByteString YamlScalar where
-    convertSuccess bs = YamlScalar bs StrTag Any
--- Why is the following required if it's in Data.Object.Dangerous?
-instance (ConvertAttempt kIn kOut, ConvertAttempt vIn vOut)
-    => FromObject (Object kOut vOut) kIn vIn where
-    fromObject = mapKeysValuesM convertAttempt convertAttempt
+    convertSuccess (JsonNumber n) = YamlScalar (cs n) FloatTag Any
+    convertSuccess (JsonBoolean True)  = YamlScalar (cs "y") BoolTag Any
+    convertSuccess (JsonBoolean False) = YamlScalar (cs "n") BoolTag Any
+    convertSuccess JsonNull = YamlScalar (cs "~") NullTag Any
+
+instance ConvertAttempt JsonDoc YamlDoc where
+    convertAttempt = fmap (cs . (convertObject :: JsonObject -> YamlObject))
+                   . ca
 
 main :: IO ()
 main = do
     args <- getArgs
     when (length args > 2) $ error "Usage: json2yaml [in] [out]"
     let (input:output:_) = args ++ repeat "-"
-    content <-
+    jsonDoc <-
         case input of
-            "-" -> BL.getContents
-            _ -> BL.readFile input
-    yo <- fa $ decode content
+            "-" -> JsonDoc `fmap` BL.getContents
+            _ -> readJsonDoc input
+    yd <- fa $ ca jsonDoc
     case output of
-        "-" -> B.putStr $ encodeYaml' (yo :: YamlObject)
-        _ -> B.writeFile output $ encodeYaml' yo
+        "-" -> B.putStr $ unYamlDoc yd
+        _ -> writeYamlDoc output yd
