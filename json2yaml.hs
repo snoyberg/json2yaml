687ea8c2344ca50e783a882742bde546c4b30d33
Michael Snoyman <michael@snoyman.com>
2010-11-30 20:52:45 +0200

    Ported to newest libraries

--- json2yaml.hs
+++ json2yaml.hs
@@ -1,34 +1,21 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
 import System.Environment (getArgs)
-import Data.Object.Json
-import Data.Object.Yaml
-import Control.Monad
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Lazy as BL
-import Data.Attempt
-
-instance ConvertSuccess JsonScalar YamlScalar where
-    convertSuccess (JsonString bs) = YamlScalar bs StrTag Any
-    convertSuccess (JsonNumber n) = YamlScalar (cs n) FloatTag Any
-    convertSuccess (JsonBoolean True)  = YamlScalar (cs "y") BoolTag Any
-    convertSuccess (JsonBoolean False) = YamlScalar (cs "n") BoolTag Any
-    convertSuccess JsonNull = YamlScalar (cs "~") NullTag Any
-
-instance ConvertAttempt JsonDoc YamlDoc where
-    convertAttempt = fmap (cs . (convertObject :: JsonObject -> YamlObject))
-                   . ca
+import Data.Object (Object)
+import qualified Data.Object.Json as J
+import qualified Data.Object.Yaml as Y
+import Control.Monad (join, when)
+import qualified Data.ByteString as S
 
 main :: IO ()
 main = do
     args <- getArgs
     when (length args > 2) $ error "Usage: json2yaml [in] [out]"
     let (input:output:_) = args ++ repeat "-"
-    jsonDoc <-
+    obj <- join $
         case input of
-            "-" -> JsonDoc `fmap` BL.getContents
-            _ -> readJsonDoc input
-    yd <- fa $ ca jsonDoc
+            "-" -> J.decode `fmap` S.getContents
+            _ -> J.decodeFile input
     case output of
-        "-" -> B.putStr $ unYamlDoc yd
-        _ -> writeYamlDoc output yd
+        "-" -> S.putStr $ Y.encode (obj :: Object S.ByteString S.ByteString)
+        _ -> Y.encodeFile output obj
